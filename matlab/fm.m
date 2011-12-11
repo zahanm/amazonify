767e68b58ebd270c1e0de0f44e9e97e3b99c3154
Zahan Malkani <zahanm@gmail.com>
2011-12-11 13:48:44 -0800

    make model3 work

--- matlab/fm.m
+++ matlab/fm.m
@@ -1,14 +1,14 @@
 clear all; close all;
 
 %% Load train
-train_data = dlmread('../data/train_matrix_7500.txt');
+train_data = dlmread('../data/pruned_matrix_train.txt');
 % shift the customer index by 1 for matlab indexing
 train_data(:, 2) = train_data(:, 2) + 1;
 
 
 %% Load test
-NUM_LINES_TEST = 7500;
-test_data = dlmread('../data/test_matrix_7500.txt', '\t', [0, 0, NUM_LINES_TEST - 1, 2]);
+NUM_LINES_TEST = 3900; % 7500;
+test_data = dlmread('../data/pruned_matrix_test.txt', '\t', [0, 0, NUM_LINES_TEST - 1, 2]);
 % shift the customer index by 1 for matlab indexing
 test_data(:, 2) = test_data(:, 2) + 1;
 
@@ -16,11 +16,12 @@ test_data(:, 2) = test_data(:, 2) + 1;
 %% Intialize model parameters
 num_products = max(train_data(:, 1));
 num_users = max(train_data(:, 2));
-latent_size = 3;
-lambda = 0.01; % regularization constant
+latent_size = 20; % k -- dimensionailty of new space
+lambda = 0.003; % regularization constant
 
 % initialize parameters
-theta = 0.001*randn((num_products + num_users)*latent_size, 1);
+% normally distributed about sqrt(4 / latent_size)
+theta = sqrt(4/latent_size) + 0.1 * randn((num_products + num_users)*latent_size, 1);
 
 %% if using fmincg
 % options = optimset('GradObj', 'on', 'MaxIter', 50);
@@ -28,8 +29,8 @@ theta = 0.001*randn((num_products + num_users)*latent_size, 1);
 %   latent_size, lambda, train_data), theta, options);
 
 %% if using minFunc
-options.Method = 'lbgfs';
-options.maxIter = 100;	
+options.Method = 'cg';
+options.maxIter = 100;
 options.display = 'on';
 addpath minFunc/
 [opt_theta, cost] = minFunc ( @(t) factorization_cost(t, num_products, num_users, ...
@@ -38,7 +39,7 @@ addpath minFunc/
 %% Results
 p = reshape(opt_theta(1:num_products*latent_size), num_products, latent_size);
 u = reshape(opt_theta(num_products*latent_size + 1: end), num_users, latent_size);
-
+p(1,:)
 train_product_idx = train_data(:, 1);
 train_user_idx = train_data(:, 2);
 train_ratings = train_data(:, 3);
