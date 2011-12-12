abbc1f6d8b5d9be006e5f5243e8b2c0fb8761c94
Zahan Malkani <zahanm@gmail.com>
2011-12-11 19:39:34 -0800

    modelthree plots

--- matlab/modelone_plots.m
+++ matlab/modelone_plots.m
@@ -1,23 +1,8 @@
-k = [1, 3, 5, 10, 25];
-x = 0:k(end);
+k = [10, 20];
+x = 5:25;
 
-%% Full set
-err_nbr = [0.34969, 0.33739, 0.334785, 0.326245, 0.322025];
-
-figure; hold on;
-plot(k, err_nbr, 'o-', 'markerfacecolor', 'b');
-plot(x, 0.3210925*ones(size(x)), 'r--'); % predict 4
-plot(x, 0.30075*ones(size(x)), 'm--'); % predict average
-
-title('Prediction performance on full Amazon dataset')
-xlabel('Number of neighbors, k');
-ylabel('Normalized RMSE');
-legend('Neighborhood model', 'Baseline: Predict 4 for all',...
-    'Baseline: Predict Average for each product')
-hold off;
-
-%% Pruned set
-err_nbr_pruned = [0.135223, 0.1379735, 0.14099425, 0.146693, 0.15489675];
+% Pruned set
+err_nbr_pruned = [0.2231, 0.2280];
 
 figure; hold on;
 plot(k, err_nbr_pruned, 'o-', 'markerfacecolor', 'b');
@@ -25,8 +10,8 @@ plot(x, 0.3049275*ones(size(x)), 'r--');
 plot(x, 0.251325*ones(size(x)), 'm--');
 
 title('Prediction performance on high-activity users and products');
-xlabel('Number of neighbors, k'); % predict 4
+xlabel('Number of latent dimensions, f'); % predict 4
 ylabel('Normalized RMSE'); % predict average
 
-legend('Neighborhood model', 'Baseline: Predict 4 for all',...
+legend('Matrix factorization model', 'Baseline: Predict 4 for all',...
     'Baseline: Predict Average for each product')
